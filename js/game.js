8f7bbc7a087da01a9a78272d9236ef68cbdf3cb5
mr.nobody <id4acc2014@protonmail.com>
2024-01-21 00:03:41 +0330

    checkAnswer

--- js/game.js
+++ js/game.js
@@ -4,9 +4,12 @@ const URL = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multip
 
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
 
 let formattedData = null;
-
+let questionIndex = 0;
+let correctAnswer = null;
 
 const fetchData = async () => {
   const response = await fetch(URL);
@@ -16,9 +19,31 @@ const fetchData = async () => {
 };
 
 const start = () => {
+  showQuestion();
   loader.style.display = "none";
   container.style.display = "block";
 };
 
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } = formattedData[questionIndex];
+  correctAnswer = correctAnswerIndex;
+  questionText.innerText = question;
+  answerList.forEach((button, index) => {
+    button.innerText = answers[index];
+  });
+};
 
-window.addEventListener("load", fetchData);
+const checkAnswer = (event, index) => {
+  const isCorrect = index === correctAnswer ? true : false;
+  if (isCorrect) {
+    event.target.classList.add("correct");
+  } else {
+    event.target.classList.add("incorrect");
+    answerList[correctAnswer].classList.add("correct");
+  }
+};
+
+// window.addEventListener("load", fetchData);
+answerList.forEach((button, index) => {
+  button.addEventListener("click", (event) => checkAnswer(event, index));
+});
